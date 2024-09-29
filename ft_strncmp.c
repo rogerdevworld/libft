4ae14b7ec4746287edcee1fc43e0fa47aa2ed495
罗杰 <rogerdevworld@gmail.com>
2024-09-29 11:27:49 +0200

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 07:32:01 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/17 08:11:45 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/29 11:27:27 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (n < i && (s1[i] || s2[i]))
+	while (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
