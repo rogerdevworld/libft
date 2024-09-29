373c16f661bbce464bd911c76a7f30e107c6c63e
罗杰 <rogerdevworld@gmail.com>
2024-09-29 12:10:23 +0200

    itoa fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,87 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/26 12:59:05 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/27 13:40:28 by rmarrero         ###   ########.fr       */
+/*   Created: 2024/09/12 17:43:29 by rmarrero          #+#    #+#             */
+/*   Updated: 2024/09/29 12:09:59 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-static int	int_len(long nbr)
+void	ft_itoa_recursive(int n, char *str, int *index)
 {
-	int	len;
-
-	if (nbr <= 0)
-		len = 1;
-	else
-		len = 0;
-	while (nbr != 0)
+	if (n == -2147483648)
 	{
-		nbr /= 10;
-		len++;
+		str[(*index)++] = '-';
+		str[(*index)++] = '2';
+		ft_itoa_recursive(147483648, str, index);
+		return ;
 	}
-	return (len);
-}
-
-static void	fill_result(long nbr, char *result, int len, int is_negative)
-{
-	int	i;
-
-	i = len - 1;
-	if (nbr == 0)
-		result[0] = '0';
-	else
+	if (n < 0)
 	{
-		if (is_negative)
-			nbr = -nbr;
-		while (nbr != 0)
-		{
-			result[i] = ((nbr % 10) + '0');
-			nbr = nbr / 10;
-			i--;
-		}
-		if (is_negative)
-			result[0] = '-';
+		str[(*index)++] = '-';
+		n = -n;
 	}
+	if (n >= 10)
+		ft_itoa_recursive(n / 10, str, index);
+	str[(*index)++] = (n % 10) + '0';
 }
 
 char	*ft_itoa(int n)
 {
-	int		len;
-	char	*result;
-	long	nbr;
+	char	*str;
+	int		index;
 
-	nbr = n;
-	len = int_len(nbr);
-	result = malloc((len + 1) * sizeof(char));
-	if (!result)
+	str = (char *)malloc(12 * sizeof(char));
+	if (!str)
 		return (NULL);
-	fill_result(nbr, result, len, n < 0);
-	result[len] = '\0';
-	return (result);
+	index = 0;
+	ft_itoa_recursive(n, str, &index);
+	str[index] = '\0';
+	return (str);
 }
-/*
-int main(void)
-{
-    int i = 0;
-    int numbers[] = {123, -456, 0, 2147483647, -2147483648};
-    char *str;
-    
-    while (i < 5)
-    {
-        str = ft_itoa(numbers[i]);
-        if (str)
-        {
-            printf("ft_itoa(%d) = %s\n", numbers[i], str);
-            free(str);
-        }
-        else
-        {
-            printf("Error al convertir el número %d\n", numbers[i]);
-        }
-        i++;
-    }
-    return 0;
-}*/
