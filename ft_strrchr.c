460c2267ac5131d6ea1c1c5e01eca998586c6c6d
罗杰 <rogerdevworld@gmail.com>
2024-09-29 11:33:59 +0200

    fixed strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 17:43:29 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/18 06:36:50 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/29 11:33:31 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -24,7 +24,7 @@ char	*ft_strrchr(const char *str, char c)
 		if (local_str[len] == (unsigned char)c)
 			return ((char *)&local_str[len]);
 	}
-	if ((unsigned char)str[len] == (unsigned char)c)
+	if (local_str[len] == (unsigned char)c)
 		return ((char *)&local_str[len]);
 	return (NULL);
 }
