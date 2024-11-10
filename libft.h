743b75c0c87577c1979a7dce4b681a93be22c800
罗杰 <rogerdevworld@gmail.com>
2024-11-10 16:23:22 +0100

    fixed bonus add header

--- libft.h
+++ libft.h
@@ -18,13 +18,7 @@
 # include <stdlib.h>
 # include <limits.h>
 
-/* Bonus struct  */
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
+/* Mandaroty */
 //void functions
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -68,4 +62,22 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s);
+
+/* Bonus */
+/* Bonus struct  */
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+/* Bonus Functions */
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
